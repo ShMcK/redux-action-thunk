957ac3fb99c0660eddaafa7822356da9fed9de90
ShMcK <shawn.j.mckay@gmail.com>
2016-05-12 21:45:10 -0700

    refactor code for readability

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,32 +5,39 @@ function createRatMiddleware() {
     dispatch,
     getState
   }) => next => action => {
-    // handle params
-    let params = null;
+
+    // handle any params
+    let params: any[] = null;
     if (action.constructor === Array) {
       params = action.slice(1);
       action = action[0];
     }
-    if (typeof action === 'string') {
-      let a = rat.actions[action];
 
-      // handle thunks
-      if (typeof a === 'function') {
-        // handle params
-        if (params && params.length) {
-          a = a(dispatch, getState)(...params);
-          return next(a);
-        }
-        return a(dispatch, getState);
-        // normal actions
-      } else if (typeof a === 'object') {
-        return next(a);
-      } else if (typeof a === 'undefined') {
-        console.log(`rat action not in list of actions. Use "rat.add('ACTION_NAME')" to add new actions: ${a}`);
-      } else {
-        console.log(`rat: invalid action dispatched. Should be an object or function but was "${typeof a}" : ${a}`);
+    // dispatch uses RAT
+    if (typeof action === 'string') {
+      action = rat.actions[action];
+
+      switch (typeof action) {
+
+        case 'object':
+          return next(action);
+
+        case 'function':
+          if (params && params.length) {
+            return next(action(dispatch, getState)(...params));
+          }
+          return action(dispatch, getState);
+
+        case 'undefined':
+          console.log(`rat action not in list of actions. Use "rat.add('ACTION_NAME')" to add new actions: ${action}`);
+          return;
+
+        default:
+          console.log(`rat: invalid action dispatched. Should be an object or function but was "${typeof action}" : ${action}`);
+          return;
       }
     }
+    // normal dispatch, do nothing
     return next(action);
   };
 }
